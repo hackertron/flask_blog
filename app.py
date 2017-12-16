e03e0c9474b60f9e794fb5e392cdab38925e9d90
hackertron <cooljay.gupta@gmail.com>
2017-12-16 19:16:24 +0530

    about

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def index():
 		return render_template('home.html')
 
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
