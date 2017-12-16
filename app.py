65f0b51bfc12025fe7848ecc1b7106c4858e0e6a
hackertron <cooljay.gupta@gmail.com>
2017-12-16 22:35:51 +0530

    session

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ class RegisterForm(Form):
 
 # User Register 
 @app.route('/register', methods=['GET', 'POST'])
-@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
@@ -76,5 +75,47 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
+
+
+# User login 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+	
+
 if __name__ == '__main__':
+	app.secret_key = 'secret123'
 	app.run(debug=True)
